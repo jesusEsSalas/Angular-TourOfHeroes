06feab678f694865fb4cf81d80d3113f01f91ea9
Jesus <jesuse.salas@alumnos.udg.mx>
2022-03-18 10:13:49 -0600

    Deploy a Heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/herokuubsmi'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
+
+app.listen(process.env.PORT || 8080)
